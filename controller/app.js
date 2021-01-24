969ec0e79894b749b1235ae62308641d165d136e
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-01-24 15:21:48 +0100

    JS: modification of sorting method -> modifying style.display of articles

--- controller/app.js
+++ controller/app.js
@@ -1,26 +1,15 @@
 const actualTagsChecked = [];
 
-// Dynamic creation of HTML based on JSON datas. Sequence of execution : A04 -> A03 -> A02 -> A01
+// _________________________________________________________________________________________________
+// DYNAMIC CREATION OF HTML BASED ON JSON DATAS. SEQUENCE OF EXECUTION : F04 -> F03 -> F02 -> F01
 
-function isTagCorrespondToTagChecked(arrayToCheck) {
-  let TrueOrFalse = false;
-  actualTagsChecked.forEach((element1) => arrayToCheck.forEach((element2) => {
-    if (element1 === element2) {
-      TrueOrFalse = true;
-    }
-  }));
-  return TrueOrFalse;
-}
-
-// A01
+// F01
 // function using template string to generate HTML, creating the photograph part of the home page
 // the argument is an object coming from the json taht include photographers datas
 // object.data is used to insert photographers information in the created HTML
 // before the <li... looping creation of tags, permit to adapt the numbers of each by photographers
 function photographHTMLGenerator(objectPhotograph) {
-  if (actualTagsChecked.length === 0 || isTagCorrespondToTagChecked(objectPhotograph.tags)) {
-    console.log('3');
-    return `
+  return `
   <article id="${objectPhotograph.id}" class="photograph">
       <!-- IMAGE LINK -->
       <div class="photograph__cadre">
@@ -40,57 +29,94 @@ function photographHTMLGenerator(objectPhotograph) {
 }
       </ul>
   </article>`;
-  }
-  return '';
 }
 
-// A02
+// F02
 // function that create all the block of HTML that is needed to be displayed
-// argument is an array, it comes from the JSON and contains photographers datas (see A03)
-// taking array of JSON, trasnform it in a new array by applying the A01 function to each (.map)
+// argument is an array, it comes from the JSON and contains photographers datas (see F03)
+// taking array of JSON, trasnform it in a new array by applying the F01 function to each (.map)
 function photographHTMLCompiler(arrayOfJson) {
   return arrayOfJson.map(photographHTMLGenerator).join('');
 }
 
-// A03
-// function applying A02 to the photographer's container
-// argument is the array coming from JSON passing to A02
+// F03
+// function applying F02 to the photographer's container
+// argument is the array coming from JSON passing to F02
 function photographCreator(data) {
   document.querySelector('.photograph-container').innerHTML = photographHTMLCompiler(data.photographers);
 }
 
-// A04
+// F04
 // function that get datas in the /src/data.json and chain with .then
-// the respons is tranform into json, then the datas received are passed to A03
+// the respons is tranform into json, then the datas received are passed to F03
 function fetchDataToCreatePhotographersHTML() {
   fetch('./controller/src/data.json')
     .then((resp) => resp.json())
-    .then(console.log('2'))
     .then((data) => photographCreator(data))
-    .then(console.log('4'))
     .catch((error) => console.log(`Erreur : ${error}`));
 }
 
-// calling A04 on loading page
+// calling F04 on loading page
 fetchDataToCreatePhotographersHTML();
 
-async function sortingByTag(tagName) {
-  const tagDOM = document.querySelector(`[data-tagName="${tagName}"]`);
+// _________________________________________________________________________________________________
+// DYNAMIC MODIFICATION WITH TAGS CLICK IN HTML -> CALL F08 | F05-6-7 IS USED FOR F08
 
+// F05
+// function modifying data-attribute isChecked to tags in all the dom, making him
+// visually checked. Argument tagname is the data-attribute tagname of the tag
+// TrueOrFalse is a string -> 'true' the tag is checked, 'false is unchecked
+function setDataCheckedAttributeOfTag(tagName, TrueOrFalse) {
+  const tagDOMS = document.querySelectorAll(`[data-tagName="${tagName}"]`);
+  tagDOMS.forEach((element) => element.setAttribute('data-isChecked', TrueOrFalse));
+}
+
+// F06
+// compare two arrays, one contain all tags checked, the other contain list of li if at least
+// one data-tagname attribute of li correspond to a tag checked, return true, else return false
+function isTagCorrespondToTagsChecked(arrayToCompare) {
+  let TrueOrFalse = false;
+  actualTagsChecked.forEach((element1) => arrayToCompare.forEach((element2) => {
+    if (element1 === element2.getAttribute('data-tagname')) {
+      TrueOrFalse = true;
+    }
+  }));
+  return TrueOrFalse;
+}
+
+// F07
+// get all articles .photograph, then get each li .tag of each articles and compared them to
+// tags that are actually checked with F06. Display 'none' for articles that don't contains
+// at least one tag checked
+function displayingArticles() {
+  // 1 select all article .photograph
+  const articlesAllDOM = document.querySelectorAll('.photograph');
+  articlesAllDOM.forEach((article) => {
+    // 2 for each, I select each li that it contain
+    const lisOfArticle = article.querySelectorAll('.tag__link--smaller');
+    // 3 making comparision + displaying
+    if (actualTagsChecked.length === 0 || isTagCorrespondToTagsChecked(lisOfArticle)) {
+      article.style.display = 'flex';
+    } else {
+      article.style.display = 'none';
+    }
+  });
+}
+
+// F08
+// receiving the calling from onclick in HTML li .tags, argument is the tagname of the li
+// then check if tag is already checked, it add/delete the tagname to actualTagsChecked[] used
+// in F06&F07, then call F05 to change checked status in HTML. Finally call F07 to display articles
+function sortingByTag(tagName) {
+  const tagDOM = document.querySelector(`[data-tagName="${tagName}"]`);
   if (tagDOM.getAttribute('data-isChecked') === 'true') {
     const pos = actualTagsChecked.indexOf(tagName);
     actualTagsChecked.splice(pos, 1);
-    fetchDataToCreatePhotographersHTML();
-    const tagDOMS = document.querySelectorAll(`[data-tagName="${tagName}"]`);
-    tagDOMS.forEach((element) => element.setAttribute('data-isChecked', 'false'));
-    // tagDOM.setAttribute('data-isChecked', 'false');
+    setDataCheckedAttributeOfTag(tagName, 'false');
   } else {
     actualTagsChecked.push(tagName);
-    (console.log('1'));
-    fetchDataToCreatePhotographersHTML();
-    (console.log('5'));
-    const tagDOMS = document.querySelectorAll(`[data-tagName="${tagName}"]`);
-    (console.log('6'));
-    tagDOMS.forEach((element) => element.setAttribute('data-isChecked', 'true'));
+    setDataCheckedAttributeOfTag(tagName, 'true');
   }
+
+  displayingArticles();
 }
