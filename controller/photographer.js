94deaed170ab9af34486834ac03124a89f4b5ce1
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-01-30 16:04:42 +0100

    JS: dynamic HTML of madie-photographer part

--- controller/photographer.js
+++ controller/photographer.js
@@ -1,4 +1,5 @@
 let actualPhotographerId = 0;
+let ActualPhotographerName = '';
 
 // _________________________________________________________________________________________________
 // DYNAMIC CREATION OF THE PAGE. POPULATE PHOTOGRAPHER & CREATA HTML FOR MEDIAS
@@ -9,36 +10,61 @@ let actualPhotographerId = 0;
 // argument: an array containing datas about each photographers from json
 // firstly, the function get data about the actual photographer using actualPhotographerId variable
 // then writing html using thd datas
-function createHtmlDescriptionPhotographer(photographers) {
-  // eslint-disable-next-line eqeqeq
-  const actualPhotographerDatas = photographers.find((el) => el.id == actualPhotographerId);
+function createHtmlDescriptionPhotographer(photographer) {
   return `
         <div class="description-photographer__text">
-          <h1 class="description-photographer__text__title">${actualPhotographerDatas.name}</h1>
-          <p class="description-photographer__text__localisation">${actualPhotographerDatas.city}, ${actualPhotographerDatas.country}</p>
-          <p class="description-photographer__text__slogan">${actualPhotographerDatas.tagline}</p>
+          <h1 class="description-photographer__text__title">${photographer.name}</h1>
+          <p class="description-photographer__text__localisation">${photographer.city}, ${photographer.country}</p>
+          <p class="description-photographer__text__slogan">${photographer.tagline}</p>
         </div>
         <div class="description-photographer__cadre">
-          <img src="../assets/src/Sample_Photos/Photographers_ID_Photos/${actualPhotographerDatas.portrait}" alt="" class="description-photographer__image">
+          <img src="../assets/src/Sample_Photos/Photographers_ID_Photos/${photographer.portrait}" alt="" class="description-photographer__image">
         </div>
         <ul class="tag tag--left" aria-label="photographer categories">
-        ${actualPhotographerDatas.tags.map((tagPhotograph) => `
+        ${photographer.tags.map((tagPhotograph) => `
         <li onclick="sortingByTag('${tagPhotograph}')" data-isChecked="" data-tagName="${tagPhotograph}" class="tag__link tag__link--smaller">#${tagPhotograph}</li>`).join('')
 }
         </ul>
         <button class="button">Contactez-moi</button>
         `;
 }
+// F06
+function createHtmlMediaPhotograph(media) {
+  // eslint-disable-next-line eqeqeq
+  if (media.photographerId == actualPhotographerId) {
+    return `
+  <article class="media-photograph">
+    <div class="media-photograph__cadre">
+        <img src="../assets/src/Sample_Photos/${ActualPhotographerName}/${media.image}" alt="" class="media-photograph__image">
+    </div>
+    <p class="media-photograph__description">${media.description}</p>
+    <p class="media-photograph__priceAndLike">${media.price}€ ${media.likes}</p>
+    <img src="../assets/src/heart-solid.svg" alt="" class="media-photograph__icon">
+  </article>
+        `;
+  }
+
+  return '';
+}
 
 // F07
+function mediaPhotographHtmlCompiler(mediaJSON) {
+  return mediaJSON.map(createHtmlMediaPhotograph).join('');
+}
+
+// F08
 // function get id from url then call functions to write html
 function photographerPageCreator(data) {
   const url = new URL(document.location.href);
   actualPhotographerId = url.searchParams.get('id');
-  document.querySelector('.description-photographer').innerHTML = createHtmlDescriptionPhotographer(data.photographers);
+  // eslint-disable-next-line eqeqeq
+  const actualPhotographerDatas = data.photographers.find((el) => el.id == actualPhotographerId);
+  ActualPhotographerName = actualPhotographerDatas.name;
+  document.querySelector('.description-photographer').innerHTML = createHtmlDescriptionPhotographer(actualPhotographerDatas);
+  document.querySelector('.media-photographer-container').innerHTML = mediaPhotographHtmlCompiler(data.media);
 }
 
-// F0
+// F09
 // function that get datas in the /src/data.json and chain with .then
 // the respons is tranform into json, then the datas received are passed to F07
 function fetchDataToCreatePhotographersHTML() {
